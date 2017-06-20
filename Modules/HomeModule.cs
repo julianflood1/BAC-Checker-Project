458d1917a8b722e49ef864820c3396730b70bca8
Alyssa Moody and David Wilson <alyssa+david@>
2017-06-20 16:39:54 -0700

    Add routing for bartender and food classes, add todos for tomorrow.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -41,6 +41,7 @@ namespace BloodAlcoholContent
         patron.AddDrinkToOrdersTable(drink);
         return View["success.cshtml"];
       };
+      //TODO: TURN INTO BAR MENU WITH FOOD, BE ABLE TO CLICK ON ITEM TO ORDER IT AND ADD SUM TO ORDER TOTAL?
       Get["/drinks"] = _ => {
         List<Drink> allDrinks = Drink.GetAll();
         return View["drinks.cshtml", allDrinks];
@@ -49,7 +50,6 @@ namespace BloodAlcoholContent
         return View["drinks_add.cshtml"];
       };
       Post["/drinks/add"] = _ => {
-        
         Drink newDrink = new Drink(Request.Form["drink-name"], Request.Form["drink-type"], Request.Form["drink-abv"], Request.Form["drink-cost"], Request.Form["drink-instances"]);
         newDrink.Save();
         return View["success.cshtml"];
@@ -58,6 +58,40 @@ namespace BloodAlcoholContent
         Drink selectedDrink = Drink.Find(parameters.id);
         return View["drink.cshtml", selectedDrink];
       };
+      Get["/bartenders"] = _ => {
+        List<Bartender> allBartenders = Bartender.GetAll();
+        return View["bartenders.cshtml", allBartenders];
+      };
+      Get["/bartenders/add"] = _ => {
+        return View["bartenders_add.cshtml"];
+      };
+      Post["/bartenders/add"] = _ => {
+        Bartender newBartender = new Bartender(Request.Form["bartender-name"]);
+        newBartender.Save();
+        return View["success.cshtml"];
+      };
+      //TODO: CREATE BARTENDER PAGE VIEW WITH ORDERS
+      Get["/bartenders/{id}"] = parameters => {
+        Bartender selectedBartender = Bartender.Find(parameters.id);
+        return View["bartender.cshtml", selectedBartender];
+      };
+      Get["/food"] = _ => {
+        List<Food> allFoods = Food.GetAll();
+        return View["food.cshtml", allFoods];
+      };
+      Get["/food/add"] = _ => {
+        return View["food_add.cshtml"];
+      };
+      Post["/food/add"] = _ => {
+        Food newFood = new Food(Request.Form["food-type"], Request.Form["food-description"], Request.Form["food-cost"]);
+        newFood.Save();
+        return View["success.cshtml"];
+      };
+      Get["/food/{id}"] = parameters => {
+        Food selectedFood = Food.Find(parameters.id);
+        return View["food.cshtml", selectedFood];
+      };
+//TODO: CREATE BARTENDER ADD FORM AND FOOD ADD FORM VIEWS
     }
   }
 }
