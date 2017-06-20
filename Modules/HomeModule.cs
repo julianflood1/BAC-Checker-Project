f2733b644e6ff63c2aa11b75287c96e86de0908e
David Wilson <davidtheadmiral@gmail.com>
2017-06-19 18:26:58 -0700

    add some basic routing-> / = index.cshtml; /patrons = patrons.cshtml; /patrons/add = the form to add a patron; /patron/{id} = specific patron. Doing this in order for front-enders to have physical copy to reference.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -12,6 +12,23 @@ namespace BloodAlcoholContent
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
+      Get["/patrons"] = _ => {
+        List<Patron> allPatrons = Patron.GetAll();
+        return View["patrons.cshtml", allPatrons];
+      };
+      Get["/patrons/add"] = _ => {
+        return View["patron_add.cshtml"];
+      };
+      Get["/patrons/{id}"] = parameters => {
+        Patron selectedPatron = Patron.Find(parameters.id);
+        return View["patron.cshtml", selectedPatron];
+      };
+
+      Post["/patrons/add"] = _ => {
+        Patron newPatron = new Patron(Request.Form["patron-name"], Request.Form["patron-gender"], Request.Form["patron-weight"], Request.Form["patron-height"]);
+        newPatron.Save();
+        return View["success.cshtml"];
+      };
     }
   }
 }
